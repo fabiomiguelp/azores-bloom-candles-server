643306f0078ce42b08b548b29023383c6f9aa72b
Fabio Pereira <fabio.ma.pereira@edu.azores.gov.pt>
2025-12-14 19:49:44 -0100

    aded prr logo

---
+++ src/admin/widgets/topbar-logo.tsx
@@ -0,0 +1,46 @@
+import { defineWidgetConfig } from "@medusajs/admin-sdk"
+
+const TopbarLogoWidget = () => (
+    <div style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "16px",
+        borderBottom: "1px solid #eaeaea",
+        backgroundColor: "#fff"
+    }}>
+        <img
+            src="/static/prr_logo.png"
+            alt="Logo"
+            style={{
+                maxHeight: "50px",
+                maxWidth: "200px"
+            }}
+        />
+    </div>
+)
+
+export const config = defineWidgetConfig({
+    zone: [
+        // Top of list pages
+        "order.list.before",
+        "customer.list.before",
+        "product.list.before",
+        "inventory_item.list.before",
+        "price_list.list.before",
+        "promotion.list.before",
+        "campaign.list.before",
+
+        // Top of detail pages
+        "order.details.before",
+        "customer.details.before",
+        "product.details.before",
+
+        // Login page
+        "login.before",
+
+        // Dashboard/Home (if available)
+        // Note: There might not be a dashboard zone in your list
+    ]
+})
+
+export default TopbarLogoWidget
